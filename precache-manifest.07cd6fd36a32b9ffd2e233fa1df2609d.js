288e31348c5decca503cfe7433695d6e067839c0
jonathan mwaniki <idolearndaily.com>
2022-08-25 16:45:40 +0300

    Updates

---
+++ precache-manifest.07cd6fd36a32b9ffd2e233fa1df2609d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6e2ff5404d78a40c2c",
+    "url": "/rotaract/static/css/main.fa2f2800.chunk.css"
+  },
+  {
+    "revision": "8d6e2ff5404d78a40c2c",
+    "url": "/rotaract/static/js/main.8d6e2ff5.chunk.js"
+  },
+  {
+    "revision": "c0ed5c63c5add1d13cf8",
+    "url": "/rotaract/static/js/1.c0ed5c63.chunk.js"
+  },
+  {
+    "revision": "26d4a10db874580b5cf0",
+    "url": "/rotaract/static/js/runtime~main.26d4a10d.js"
+  },
+  {
+    "revision": "ce67dba53d2dc245c27c5fb890796223",
+    "url": "/rotaract/static/media/bg1.ce67dba5.jpg"
+  },
+  {
+    "revision": "bbda841f1e7e012d2661c0c7a83a65c5",
+    "url": "/rotaract/static/media/DSC_8494.bbda841f.jpg"
+  },
+  {
+    "revision": "8afc0b2ad088ad8e3e524e7cfbd02b68",
+    "url": "/rotaract/index.html"
+  }
+];
